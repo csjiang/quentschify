58c82f97aa95f2e21cfbcc2e3369b1162599a388
C.S. Jiang <christine.s.jiang@gmail.com>
2017-05-05 16:37:00 -0400

    update template for styles and cors

--- client/index.js
+++ client/index.js
@@ -4,7 +4,8 @@ import { Router, browserHistory } from 'react-router';
 
 // Import your routes so that you can pass them to the <Router /> component
 import routes from './routes.js';
+import { useBasename } from 'history';
 
 render((
-  <Router routes={routes} history={browserHistory} />
+  <Router routes={routes} history={useBasename(() => browserHistory)({ basename: process.env.PUBLIC_URL })} />
 ), document.getElementById('root'));
